a1d9b07f87053088eb8cdf95bb010f74a00c8c9e
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-18 16:01:05 -0700

    Implemented create user functionality
    Added some basic middleware to handle a few edge cases

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -10,7 +10,60 @@ async function read(req, res, next) {
   res.json({ data });
 }
 
+async function create(req, res, next) {
+  users_service
+    .create(req.body)
+    .then((data) => res.status(201).json({ data }))
+    .catch(next);
+}
+
 // MIDDLEWARE BELOW //
+
+function validateUsername(req, res, next) {
+  if (req.body.username.length < 4 || req.body.username.length > 20) {
+    return next({
+      status: 404,
+      message: "Username must be between 4 - 20 characters",
+    });
+  } else if (req.body.username.indexOf(" ") >= 0) {
+    return next({
+      status: 404,
+      message: "Username cannot contain spaces",
+    });
+  } else {
+    return next();
+  }
+}
+
+function validatePassword(req, res, next) {
+  if (req.body.password.length < 4 || req.body.password.length > 20) {
+    return next({
+      status: 404,
+      message: "Password must be between 4 - 20 characters",
+    });
+  } else if (req.body.password.indexOf(" ") >= 0) {
+    return next({
+      status: 404,
+      message: "Password cannot contain spaces",
+    });
+  } else {
+    return next();
+  }
+}
+
+function validateFullName(req, res, next) {
+  if (
+    req.body.first_name.indexOf(" ") >= 0 ||
+    req.body.last_name.indexOf(" ") >= 0
+  ) {
+    return next({
+      status: 404,
+      message: "First & last name cannot contain spaces.",
+    });
+  } else {
+    return next();
+  }
+}
 
 async function userExists(req, res, next) {
   const username = req.params.username;
@@ -28,6 +81,7 @@ async function userExists(req, res, next) {
 }
 
 module.exports = {
+  create: [validateUsername, validatePassword, validateFullName, create],
   list,
   read: [userExists, read],
 };
--- src/users/users.service.js
+++ src/users/users.service.js
@@ -1,5 +1,12 @@
 const knex = require("../db/connection");
 
+function create(user) {
+  return knex("users")
+    .insert(user)
+    .returning("*")
+    .then((records) => records[0]);
+}
+
 function list() {
   return knex("users").select("*");
 }
@@ -9,6 +16,7 @@ function read(username) {
 }
 
 module.exports = {
+  create,
   list,
   read,
 };
