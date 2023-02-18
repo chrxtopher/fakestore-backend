7b41ddc94b4315b22099beaf44551074863276a7
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-18 14:27:46 -0700

    Addressed a typo

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const { PORT = 4050 } = process.env;
 const app = require("./app");
 
-const listener = () => console.log(`Linstening on port : ${PORT}`);
+const listener = () => console.log(`Listening on port : ${PORT}`);
 app.listen(PORT, listener);
