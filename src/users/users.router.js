d85147eff7c06f5a8b9a61768a42642e997a2354
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-18 16:00:02 -0700

    Added routing for creating a new user

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const controller = require("./users.controller");
 
 router.route("/").get(controller.list);
+router.route("/signup").post(controller.create);
 router.route("/:username").get(controller.read);
 
 module.exports = router;
