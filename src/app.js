2a426a63d1735f9effae5d6225bce27648cf4a58
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-18 14:28:19 -0700

    Implemented error handlers and users functionality

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+const usersRouter = require("./users/users.router");
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/users", usersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
