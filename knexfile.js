429ace28dcbf33b96c98c080dbaaef87a4e8b54f
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-17 18:52:23 -0700

    connected knex to the development database

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
+const { DEV_DATABASE_URL } = process.env;
+
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "",
+    connection: DEV_DATABASE_URL,
   },
 
   production: {
