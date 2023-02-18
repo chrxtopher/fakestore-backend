21f7c49af693ab4832e0d561343826a80b2329f7
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-18 13:18:43 -0700

    knex migration for createUsersTable now creates the table

--- src/db/migrations/20230218022318_createUsersTable.js
+++ src/db/migrations/20230218022318_createUsersTable.js
@@ -2,14 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTableIfNotExists("users", (table) => {
+    table.increments("user_id").primary().notNullable();
+    table.string("username").notNullable();
+    table.string("password").notNullable();
+    table.string("first_name");
+    table.string("last_name");
+    table.specificType("wishlist", "INT[]");
+    table.specificType("cart", "INT[]");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
 };
