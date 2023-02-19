341add98baf78b3328afd13785e705f60d9dfc75
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-19 00:02:30 -0700

    Changed a few routes for HTTP requests

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const controller = require("./users.controller");
 
-router.route("/").get(controller.list);
-router.route("/signup").post(controller.create);
-router.route("/:username").get(controller.read);
+router.route("/").get(controller.list).post(controller.create);
+router.route("/:username").get(controller.read).delete(controller.delete);
 
 module.exports = router;
