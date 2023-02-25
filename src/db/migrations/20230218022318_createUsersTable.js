9e0c07095c8d38cbb4c0978463029447c77d4552
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-25 15:39:53 -0700

    Changed 'cart' column to have a json field

--- src/db/migrations/20230218022318_createUsersTable.js
+++ src/db/migrations/20230218022318_createUsersTable.js
@@ -10,7 +10,7 @@ exports.up = function (knex) {
     table.string("first_name");
     table.string("last_name");
     table.specificType("wishlist", "INT[]");
-    table.specificType("cart", "INT[]");
+    table.jsonb("cart");
   });
 };
 
