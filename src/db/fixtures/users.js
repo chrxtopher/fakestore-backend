306ba145f524ff229c940b6c4f280631d516bd7e
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-25 15:40:14 -0700

    Changed seed data for each user to input an array of objects for cart

--- src/db/fixtures/users.js
+++ src/db/fixtures/users.js
@@ -5,7 +5,11 @@ module.exports = [
     first_name: "Christopher",
     last_name: "Roddy",
     wishlist: [1, 2, 3, 4],
-    cart: [1, 2],
+    cart: JSON.stringify([
+      { productId: 1, quantity: 2 },
+      { productId: 2, quantity: 1 },
+      { productId: 3, quantity: 4 },
+    ]),
   },
   {
     username: "mands",
@@ -13,6 +17,10 @@ module.exports = [
     first_name: "Amanda",
     last_name: "Burhop",
     wishlist: [4, 5],
-    cart: [1, 2, 3],
+    cart: JSON.stringify([
+      { productId: 4, quantity: 1 },
+      { productId: 5, quantity: 3 },
+      { productId: 6, quantity: 7 },
+    ]),
   },
 ];
